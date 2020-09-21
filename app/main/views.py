1208961610cb0365d1ec9abb03e4f94c16799e79
Dennis Kamunya <dennismuriithik@gmail.com>
2020-09-21 16:38:55 +0300

    Create load user fn

--- app/main/views.py
+++ app/main/views.py
@@ -1,8 +1,10 @@
 from flask import render_template,request,redirect,url_for
 from . import main
+from flask_login import login_required
 
 # Views
 @main.route('/')
+@login_required
 def index():
 
     '''
--- app/models.py
+++ app/models.py
@@ -1,8 +1,15 @@
 from . import db
 from datetime import datetime
 from werkzeug.security import generate_password_hash,check_password_hash
+from flask_login import UserMixin
+from . import login_manager
 
-class User(db.Model):
+
+@login_manager.user_loader
+def load_user(user_id):
+    return User.query.get(int(user_id))
+    
+class User(UserMixin,db.Model):
 
     'User model schema'
 
