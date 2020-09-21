46d1ec5f3da8d6e5e3141bf6ee75c3d0180fb05d
Dennis Kamunya <dennismuriithik@gmail.com>
2020-09-21 12:34:08 +0300

    Set up SQLAlchemy database uri configuration

--- config.py
+++ config.py
@@ -4,7 +4,7 @@ class Config:
     General configuration parent class
     '''
     SECRET_KEY = os.environ.get('SECRET_KEY')
-
+    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://kinc:kinc@localhost/pitch_deck'
 class ProdConfig(Config):
     '''
     Production  configuration child class
