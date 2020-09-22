bc98c85a73b92bfe0e2649823b530c51591c0273
Dennis Kamunya <dennismuriithik@gmail.com>
2020-09-22 11:20:47 +0300

    Update register view fn to register user with default profile_pic_path

--- app/auth/views.py
+++ app/auth/views.py
@@ -9,7 +9,7 @@ from .forms import LoginForm,RegistrationForm
 def register():
     form = RegistrationForm()
     if form.validate_on_submit():
-        user = User(email = form.email.data, username = form.username.data,password = form.password.data)
+        user = User(email = form.email.data, username = form.username.data,password = form.password.data,profile_pic_path= 'photos/unknown.png')
         db.session.add(user)
         db.session.commit()
         return redirect(url_for('auth.login'))
