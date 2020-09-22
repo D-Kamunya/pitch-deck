ae93d1d2f38680810a619bf7871436311bc75c4d
Dennis Kamunya <dennismuriithik@gmail.com>
2020-09-22 10:17:58 +0300

    Create update profile form and view fn

--- app/main/views.py
+++ app/main/views.py
@@ -2,6 +2,8 @@ from flask import render_template,request,redirect,url_for,abort
 from . import main
 from flask_login import login_required
 from ..models import User
+from .forms import UpdateProfile
+from .. import db
 # Views
 @main.route('/')
 @login_required
@@ -26,3 +28,21 @@ def profile(username):
 
     return render_template("profile/profile.html", user = user)    
 
+@main.route('/profile/<username>/update',methods = ['GET','POST'])
+@login_required
+def update_profile(username):
+    user = User.query.filter_by(username = username).first()
+    if user is None:
+        abort(404)
+
+    form = UpdateProfile()
+
+    if form.validate_on_submit():
+        user.bio = form.bio.data
+
+        db.session.add(user)
+        db.session.commit()
+
+        return redirect(url_for('.profile',username=user.username))
+
+    return render_template('profile/update.html',form =form)
